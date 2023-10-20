9af543712c1b8aaa991246c46edc85fe359a85eb
safa ayachi <safa.ayachi@polytechnicien.tn>
2023-10-20 22:08:21 +0100

    fixing icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,9 +47,10 @@ const Home: NextPage = () => {
 									FIND ME ON
 								</div>
 								<div className="flex flex-row justify-start items-center py-4 space-x-4">
-									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out"></div>
-									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out"></div>
-									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out"></div>
+									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out flex justify-center items-center"><i className="icon-github text-white text-xs"></i></div>
+									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out flex justify-center items-center"><i className="icon-linkedin text-white text-xs"></i></div>
+									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out flex justify-center items-center"><i className="icon-stackoverflow text-white text-xs flex justify-center items-center"></i></div>
+									<div className="h-8 w-8 bg-secondary rounded-md shadow-md cursor-pointer hover:scale-125 transition duration-700 ease-in-out flex justify-center items-center"><i className="icon-skype text-white text-xs"></i></div>
 								</div>
 							</div>
 							<div className=" w-1/2  flex justify-end items-start px-4">
